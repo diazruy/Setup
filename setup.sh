d30d7955d4d31b8c793bd5990911edbb4082d896
Ruy Diaz <diazruy@ubuntu.(none)>
2011-05-01 22:45:09 -0700

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,28 @@
-sudo apt-get install build-essential gnome-do \
+#! /bin/sh
+sudo add-apt-repository ppa:jerome-etienne/neoip && \
+  sudo apt-get update && \
+  sudo apt-get install build-essential gnome-do \
   git-core git-cola ruby-dev rubygems rake curl vim-gnome \
-  exuberant-ctags screen libopenssl-ruby && \
-  bash < <(curl -s https://rvm.beginrescueend.com/install/rvm) && \
+  exuberant-ctags screen libopenssl-ruby \
+  bison openssl libreadline6 libreadline6-dev  \
+  zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 \
+  libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf \
+  libc6-dev ncurses-dev ncurses-term nodejs && \
+ bash < <(curl -s https://rvm.beginrescueend.com/install/rvm) && \
+  echo "Finish RVM installation (add if and source rvm in shells in .bashrc)" && read -p "Press any key to continue" && \
   rvm install package zlib && \
   rvm install 1.9.2 --with-zlib-dir=$rvm_path/usr --with-readline-dir="$HOME/.rvm/usr" && \
   ssh-keygen -t rsa -C "diazruy@gmail.com" && \
-  cat id_rsa.pub && echo "Intall SSH Key in github" && pause && \
+  cat ~/.ssh/id_rsa.pub && echo "Intall SSH Key in github" && read -p "Press any key to continue" && \
   git clone git@github.com:diazruy/dotvim.git ~/.vim && \
-  ls -s ~/.vim/vimrc ~/.vimrc && \
+  ln -s ~/.vim/vimrc ~/.vimrc && \
   cd ~/.vim && \
   git submodule init && git submodule update && \
-  cd bundle/command-t && rvm use system && rake make
+  cd bundle/command-t && rvm use system && rake make && \
+  read -p "Add 'export TERM=xterm-256color' to .bashrc" && \
+  mkdir ~/projects && cd ~/projects && rvm use 1.9.2 &&
+  gem install heroku bundler --no-ri --no-rdoc && heroku keys:add && \
+  git clone -o heroku git@heroku.com:splitsee.git && \
+  rvm gemset create splitsee && cd splitsee && bundle
+  
 

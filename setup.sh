2ce60f4c9db8040f7c1a72a4a1ae33ed7b181254
Ruy Diaz <ruy@ubuntu.(none)>
2011-05-08 22:50:16 -0700

    Tweak to gem installation

--- setup.sh
+++ setup.sh
@@ -24,7 +24,8 @@ sudo add-apt-repository ppa:jerome-etienne/neoip && \
   mkdir ~/projects && cd ~/projects && rvm use 1.9.2 &&
   gem install heroku bundler --no-ri --no-rdoc && heroku keys:add && \
   git clone -o heroku git@heroku.com:splitsee.git && \
-  rvm gemset create splitsee && cd splitsee && bundle && rake db:migrate && \
+  rvm gemset create splitsee && cd splitsee && gem install bundler && \
+  bundle && rake db:migrate && \
   rake db:test:prepare
   
 

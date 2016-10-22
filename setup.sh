b3609674c2ada49d5784b91eb16b95fafacd3371
Ruy Diaz <diazruy@users.noreply.github.com>
2016-10-22 13:18:25 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -60,24 +60,32 @@ sudo apt-get install \
 
   # Run "indicator" from unity
   # Run "tweak tool" from unity and change Caps to Control: Typing / Caps Lock Behaviour / Make Caps lock extra Ctrl
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" \
-  chsh -s /bin/zsh \
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
+  chsh -s /bin/zsh &&
+  
+  git clone git@github.com:diazruy/dotfiles.git &&
+  cd ~/.dotfiles &&
+  ./setup.sh &&
+  
+  git clone git@github.com:diazruy/dotvim.git ~/.vim &&
+  ln -s ~/.vim/.vimrc ~/.vimrc &&
+  cd ~/.vim
+  git submodule init
+  git submodule update
   
   # RVM
   curl -sSL https://get.rvm.io | bash -s stable --with-default-gems="rails haml" \
   rvm install 2.3.0 && \
+
+  sudo -u postgres create user -s rew -P &&
   
+  wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh &&
+  heroku login && 
+    
   # SSH
   ssh-keygen -t rsa -C "diazruy@gmail.com" && \
   cat ~/.ssh/id_rsa.pub && echo "Intall SSH Key in github" && \
-  read -p "Press any key to continue" && \
-  google-chrome http://github.com/diazruy && \
-  # Vim
-  git clone git@github.com:diazruy/dotvim.git ~/.vim && \
-  ln -s ~/.vim/vimrc ~/.vimrc && \
-  cd ~/.vim && \
-  git submodule init && git submodule update && \
-  cd bundle/command-t && rvm use system && rake make && \
+  
   read -p "Add 'export TERM=xterm-256color' to .bashrc" && \
   
 
